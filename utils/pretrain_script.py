82838d655fe04d36e89f491842f28ac8b2ae57b3
Taylor Kirk <tkirk@sandiego.edu>
2026-02-05 15:55:50 -0800

    added pretrain script and updated main notebook with how to use it

---
+++ utils/pretrain_script.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from datasets import Dataset, DatasetDict
+
+def train_prep(df: pd.DataFrame, test1: float=0.3, test2: float=0.5):
+    # Label mapping
+    label2id = {label: idx for idx, label in enumerate(df['source'].unique())}
+    id2label = {idx: label for label, idx in label2id.items()}
+
+    # Prepare dataset
+    dataset = Dataset.from_dict({
+        "text": df['comment_text'].tolist(),
+        "label": [label2id[source] for source in df['source']]
+    })
+
+    # Split dataset 3-ways and combine back into  one
+    split_dataset = dataset.train_test_split(test_size=test1, seed=10)
+    test_valid = split_dataset['test'].train_test_split(test_size=test2, seed=10)
+
+    final_dataset = DatasetDict({
+        "train": split_dataset['train'],
+        "test": test_valid['test'],
+        "valid": test_valid['train']
+    })
+
+    return final_dataset, label2id, id2label
